d3d09ce569c796f9b3b3e5d7b73ab6969ee6640c
Ameer Sadek <66375432+AmeerSadek@users.noreply.github.com>
2024-11-17 19:19:07 +0400

    Create order endpoint and service
    Co-authored-by: Ameer Sadek <a.sadek@mailahoy.com>

---
+++ src/OrdersService/Application/Contracts/Dtos/CreateOrderInputDto.cs
@@ -0,0 +1,7 @@
+﻿namespace OrdersService.Application.Contracts.Dtos;
+
+public class CreateOrderInputDto
+{
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ src/OrdersService/Application/Services/OrdersService.cs
@@ -0,0 +1,12 @@
+﻿using OrdersService.Application.Contracts.Dtos;
+using OrdersService.Application.Contracts.Interfaces;
+
+namespace OrdersService.Application.Services;
+
+public class OrdersService : IOrdersService
+{
+    public Task<CreateOrderOutputDto> CreateOrderAsync(CreateOrderInputDto createOrderDto)
+    {
+        throw new NotImplementedException();
+    }
+}
